332d29b9427d20329f2b81ca73660692eec1a753
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-14 19:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let n = str.length;
+	for(int i=0; i<n; i++){
+		let found = true;
+		for(let j=0; j<n; j++){
+			if(i !== j && str[i] === str[j]){
+				found = false;
+				break;
+			}
+		}
+		if(found){
+			return i;
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
